67a7b3b7bb1cc038cf67dc9dbf5b5fde88281ac0
Gabriel Raby <grabyc@gmail.com>
2021-05-30 17:14:16 -0300

    PW2 - Refactoring de validaciones con express-validator

--- src/handlers/users/add.js
+++ src/handlers/users/add.js
@@ -1,44 +1,47 @@
 const moment = require("moment");
 const database = require("../../database");
+const { body, validationResult } = require("express-validator");
 
 module.exports = (route) => {
-  route.post("/", (req, res) => {
-    const newUser = {
-      name: req.body.name,
-      age: req.body.age,
-      created: moment(new Date()).format("YYYY-MM-DD"),
-    };
+  route.post(
+    "/",
+    body("name").notEmpty().withMessage("Campo obligatorio"),
+    body("age")
+      .notEmpty()
+      .withMessage("Campo obligatorio")
+      .isInt({ min: 18, max: 120 })
+      .withMessage(
+        "El valor debe ser un número y debe estar comprendido entre 18 y 120"
+      ),
 
-    const userIndex = database.DB.findIndex(
-      (item) => item.name === newUser.name
-    );
+    (req, res) => {
+      const errors = validationResult(req);
 
-    if (userIndex === -1) {
-      if (!(newUser.name === "" || newUser.age === "")) {
-        // TRUE only if name <> "" && age <> ""
-        if (newUser.age >= 18 && newUser.age <= 120) {
-          // TRUE only if name <> "" && age <> ""
+      if (!errors.isEmpty()) {
+        res.status(400).json({ errors: errors.array() });
+        return;
+      }
 
-          database.add(newUser);
-          res.status(201).json({
-            message: "Elemento nuevo agregado.",
-          });
-        } else {
-          res.status(400).json({
-            message:
-              "Elemento no cumple con las valicaciones de edad requeridas.",
-          });
-        }
+      const newUser = {
+        name: req.body.name,
+        age: req.body.age,
+        created: moment(new Date()).format("YYYY-MM-DD"),
+      };
+
+      const userIndex = database.DB.findIndex(
+        (item) => item.name === newUser.name
+      );
+
+      if (userIndex === -1) {
+        database.add(newUser);
+        res.status(201).json({
+          message: "Elemento nuevo agregado.",
+        });
       } else {
-        res.status(400).json({
-          message:
-            "Elemento no cumple con las validaciones de nombre y edad requeridas.",
+        res.status(409).json({
+          message: "Elemento duplicado no será agregado.",
         });
       }
-    } else {
-      res.status(409).json({
-        message: "Elemento duplicado no será agregado.",
-      });
     }
-  });
+  );
 };
